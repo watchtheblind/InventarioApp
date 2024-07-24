428532d23acf293d09167c5995eb8a6a89504835
Luis Monasterios <ljmm2k2@gmail.com>
2024-07-24 13:45:07 -0400

    finalizar diseño del login

--- cliente/src/app/login/page.tsx
+++ cliente/src/app/login/page.tsx
@@ -7,30 +7,34 @@ export default function Login() {
     <main className="bg-[#26313c] h-screen flex items-center justify-center p-10">
       <div className="grid w-full h-full grid-cols-1 bg-white md:grid-cols-2">
         <div className="bg-[#16202a] text-white flex items-center justify-center flex-col">
-          <div className="my-4">
-            <h1 className="text-3xl font-semibold ">Inicio de Sesión</h1>
-            <p className="mt-2 text-xs text-slate-400">
-              introduzca sus datos para acceder.
+          <div className="my-7">
+            <h1 className="text-4xl font-semibold ">Inicio de Sesión</h1>
+            <p className="mt-2 flex justify-center text-xs text-slate-400">
+              introduzca sus datos para acceder
             </p>
           </div>
           <form>
-            <Label htmlFor="email">Introduzca su correo electrónico</Label>
+            <Label htmlFor="email" className="text-base">
+              Correo
+            </Label>
             <Input
-              className="mt-2 mb-4 bg-transparent rounded-full"
+              className="mt-2 mb-5 w-80 bg-transparent rounded-full"
               type="email"
               id="email"
-              placeholder="email"
+              placeholder="introduzca su correo aquí"
             />
-            <Label htmlFor="password">Introduzca su contraseña</Label>
+            <Label htmlFor="password" className="text-base">
+              Contraseña
+            </Label>
             <Input
-              className="mt-2 mb-4 bg-transparent rounded-full"
+              className="mt-2 mb-5 w-80 bg-transparent rounded-full"
               type="password"
               id="password"
-              placeholder="contraseña"
+              placeholder="introduzca su contraseña aquí"
             />
             <Button
               type="submit"
-              className="w-full mt-5 bg-indigo-600 rounded-full hover:bg-indigo-700">
+              className="w-full mt-5 bg-[#5C776B] rounded-full hover:bg-[#475D53]">
               Iniciar Sesión
             </Button>
           </form>
