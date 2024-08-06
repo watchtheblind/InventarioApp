9adc5149a40154b529d5bedad5b449d817e99ef1
Luis Monasterios <ljmm2k2@gmail.com>
2024-08-06 15:00:45 -0400

    crear formulario de crear productos y su esquema de validación

---
+++ src/app/hooks/EsquemaProductos.ts
@@ -0,0 +1,30 @@
+import {z} from 'zod'
+
+const EsquemaProductos = z.object({
+  nombre: z
+    .string()
+    .min(1, {message: 'El campo no puede estar vacío'})
+    .max(50, {message: 'El nombre no debe exceder de 50 caracteres'}),
+  precio: z
+    .number({
+      required_error: 'Un número es requerido',
+      invalid_type_error: 'La cantidad debe ser un número',
+    })
+    .positive()
+    .safe(),
+  _id: z
+    .string()
+    .min(10, {message: 'La descripción debe tener al menos 10 caracteres'})
+    .max(50, {message: 'La descripción no debe exceder de 50 caracteres'}),
+  descripcion: z
+    .string()
+    .min(10, {message: 'La descripción debe tener al menos 10 caracteres'})
+    .max(50, {message: 'La descripción no debe exceder de 50 caracteres'}),
+  cantidad: z
+    .number({
+      required_error: 'Un número es requerido',
+      invalid_type_error: 'La cantidad debe ser un número',
+    })
+    .positive()
+    .safe(),
+})
--- src/app/modulos/productos/modalCrearProducto.tsx
+++ src/app/modulos/productos/modalCrearProducto.tsx
@@ -1,8 +1,7 @@
-import {Button} from '@/components/ui/button'
+'use client'
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -10,33 +9,47 @@ import {
 } from '@/components/ui/dialog'
 import {Input} from '@/components/ui/input'
 import {Label} from '@/components/ui/label'
+import {Button} from '@/components/ui/button'
+import {useForm} from 'react-hook-form'
 type Props = {
   Boton: any
 }
 export const ModalCrearProducto: React.FC<Props> = (props) => {
+  const {register, handleSubmit} = useForm()
   return (
     <Dialog>
       <DialogTrigger asChild>{props.Boton}</DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when youre done.
-          </DialogDescription>
+          <DialogTitle className="text-center">
+            Crear producto nuevo
+          </DialogTitle>{' '}
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+          <form
+            onSubmit={handleSubmit((datos) => {
+              console.log(datos)
+            })}>
+            <Label htmlFor="producto-nombre">Nombre</Label>
+            <Input id="producto-nombre" {...register('nombre')}></Input>
+            <Label htmlFor="producto-precio">Precio</Label>
+            <Input
+              type="number"
+              id="producto-precio"
+              {...register('precio')}></Input>
+            <Label htmlFor="producto-id">Serial/Id</Label>
+            <Input id="producto-id" {...register('_id')}></Input>
+            <Label htmlFor="producto-descripcion">Descripcion</Label>
+            <Input
+              id="producto-descripcion"
+              {...register('descripcion')}></Input>
+            <Label htmlFor="producto-cantidad">Cantidad de unidades</Label>
+            <Input
+              type="number"
+              id="producto-cantidad"
+              {...register('cantidad')}></Input>
+            <Button type="submit"></Button>
+          </form>
         </div>
         <DialogFooter>
           <Button type="submit">Save changes</Button>
