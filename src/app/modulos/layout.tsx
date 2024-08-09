07aa579f84a4d5cf27a4f3609ce3832adc30d338
Luis Monasterios <ljmm2k2@gmail.com>
2024-08-09 15:52:23 -0400

    crear modelos de barra lateral y de navegación

--- src/app/modulos/layout.tsx
+++ src/app/modulos/layout.tsx
@@ -1,11 +1,165 @@
-'use client'
-import {Nav} from '@/components/nav/nav'
+import Link from 'next/link'
+import {
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+} from 'lucide-react'
 
-export default function ModulosLayout({children}: {children: React.ReactNode}) {
+import {Badge} from '@/components/ui/badge'
+import {Button} from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {Input} from '@/components/ui/input'
+import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
+
+export default function Dashboard({children}: {children: React.ReactNode}) {
   return (
-    <main className="w-full">
-      <Nav></Nav>
-      {children}
-    </main>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">InventarioApp</span>
+            </Link>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="./dashboard"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                <Home className="h-4 w-4" />
+                Panel Principal
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                <ShoppingCart className="h-4 w-4" />
+                Orders
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="./productos"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
+                <Package className="h-4 w-4" />
+                Productos{' '}
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                <Users className="h-4 w-4" />
+                Proveedores
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                <LineChart className="h-4 w-4" />
+                Analytics
+              </Link>
+            </nav>
+          </div>
+          <div className="mt-auto p-4"></div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden">
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Mostrar menú de navegación</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold">
+                  <Package2 className="h-6 w-6" />
+                  <span className="sr-only">InventarioApp</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+                  <Home className="h-5 w-5" />
+                  Panel Principal
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground">
+                  <ShoppingCart className="h-5 w-5" />
+                  Orders
+                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                    6
+                  </Badge>
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+                  <Package className="h-5 w-5" />
+                  Productos
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+                  <Users className="h-5 w-5" />
+                  Proveedores
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+                  <LineChart className="h-5 w-5" />
+                  Analytics
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <div className="w-full flex-1"></div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Mostrar menú de usuario</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Ajustes</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Salir</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          {children}
+        </main>
+      </div>
+    </div>
   )
 }
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,33 +1,30 @@
 import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar'
-
+import {Button} from '../ui/button'
 export function Nav() {
   return (
-    <nav className="h-auto flex w-full text-white">
-      <article className="bg-blue-800 flex justify-between flex-auto py-5 2xl:px-4 xl:px-4 lg:px-4 md:px-3 sm:px-2 px-2 w-full">
-        <button className="2xl:text-base xl:text-base lg:text-base md:text-sm text-sm">
+    <header className="h-20 flex justify-between bg-blue-800 text-white">
+      <div className="flex justify-start pl-4 xl:pl-4 lg:pl-4 md:pl-3 sm:pl-2">
+        <Button className="text-base xl:text-base lg:text-base md:text-sm">
           Dez
+        </Button>
+      </div>
+      <div className="flex justify-end pr-4 xl:pr-4,pl-4 lg:pr-4, md:pr-3 sm:pr-2 ">
+        <ul className="flex gap-5 items-center">
+          <li className="flex items-center">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </li>
+          <li className="text-base xl:text-base lg:text-base md:text-sm ">
+            Nombre - Administrador
+          </li>
+        </ul>
+        <button className="text-base ml-6 xl:text-base lg:text-base md:text-sm">
+          LOGOUT
         </button>
-        <div className="flex gap-8 items-center">
-          <ul className="flex gap-3 items-center">
-            <li className="2xl:text-base xl:text-base lg:text-base md:text-sm text-sm">
-              <Avatar>
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </li>
-            <li className="2xl:text-base xl:text-base lg:text-base md:text-sm text-sm">
-              Nombre - Adminstrador
-            </li>
-          </ul>
-          <button className="2xl:text-base xl:text-base lg:text-base md:text-sm text-sm">
-            LOGOUT
-          </button>
-        </div>
-      </article>
-    </nav>
+      </div>
+    </header>
   )
 }
 export default Nav
